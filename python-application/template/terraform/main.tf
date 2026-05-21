96b789c9e801f0c3383bb4b1f5df865b8d5dd661
Abdulquadri Olatunbosun <olaolat@hotmail.com>
2026-05-21 12:08:59 +0100

    modify repo name

--- python-application/template/terraform/main.tf
+++ python-application/template/terraform/main.tf
@@ -1,11 +1,5 @@
-variable "repository_name" {
-  description = "Name of the ECR public repository"
-  type        = string
-  default     = "python-application"
-}
-
 resource "aws_ecrpublic_repository" "python_application" {
-  repository_name = "idp/${var.repository_name}"
+  repository_name = "idp/${{values.app_name}}"
 
   catalog_data {
     description = "Python app that displays time"
